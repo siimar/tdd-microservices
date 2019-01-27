6538417dee6cec2155f730d8d2adf63bc8fcfa7d
Siim <foobar8@gmail.com>
2019-01-27 22:33:19 +0200

    part 1 test

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 
 # set config
 app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+#app.config.from_object(app_settings)
+app.config.from_object('project.config.DevelopmentConfig')
+
 
 # instantiate the db
 db = SQLAlchemy(app)
