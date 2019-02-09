b0f0d190704baf6f4b96a7f390cface16cc1c54c
Siim <foobar8@gmail.com>
2019-02-09 20:14:59 +0200

    part 2 React Forms

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+import AddUser from './components/AddUser';
 import UsersList from './components/UsersList';
 
 
@@ -9,8 +10,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
   componentDidMount() {
     this.getUsers();
@@ -18,16 +23,41 @@ class App extends Component {
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
-    .catch((err) => {console.log(err); });
+    .catch((err) => { console.log(err); });
+  };
+    addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({ username: '', email: ''})
+      })
+      .catch((err) => { console.log(err) });
+  };
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   };
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br/>
               <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <hr/><br/>
               <UsersList users={this.state.users}/>
             </div>
